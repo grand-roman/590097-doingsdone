fd1e219fb9403130026a5adb0797e7c7d8080eaf
Roman <31464179+grand-roman@users.noreply.github.com>
2019-02-15 09:16:07 +0700

    fix

--- index.php
+++ index.php
@@ -2,11 +2,19 @@
 
 require_once("functions.php");
 require_once("data.php");
+require_once("mysqli_helper.php");
+
+if(file_exists('config.php')) {
+    require_once 'config.php';
+} else {
+    require_once 'config.default.php';
+}
 
 
-$connect = mysqli_connect("localhost", "root", "", "doingsdone");
+//$connect = mysqli_connect("localhost", "root", "", "doingsdone");
+$connect = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-
+ $user_id = 1;
 
 if ($connect === false) {
     $error = mysqli_error($connect);
@@ -17,8 +25,9 @@ if ($connect === false) {
     mysqli_set_charset($connect, "utf8");
 
     //SQL-запрос для получения списка проектов у текущего пользователя
-    $sql = "SELECT id, name_project FROM Project  WHERE user_id = " . 1 ;
-    $result = mysqli_query($connect, $sql);
+    $sql = "SELECT id, name_project FROM Project  WHERE user_id = " $user_id ;
+    $stmt = db_get_prepare_stmt($link, $sql, [$data]);
+    $result = mysqli_query($connect, $stmt);
     if (!$result) {
         $error = mysqli_error($connect);
         print("Ошибка MySQL:" .$error);
@@ -28,8 +37,9 @@ if ($connect === false) {
 
     //SQL-запрос для получения списка из всех задач у текущего пользователя
     $sql =  "SELECT name_task, file_task, done_at, deadline, status, project_id
-            FROM Task  where project_id =" . 1;
-    $result = mysqli_query($connect, $sql);
+            FROM Task  where project_id =" . $user_id;
+    $stmt = db_get_prepare_stmt($link, $sql, [$data]);
+    $result = mysqli_query($connect, $stmt);
     if (!$result) {
         $error = mysqli_error($connect);
         print("Ошибка MySQL:" .$error);
